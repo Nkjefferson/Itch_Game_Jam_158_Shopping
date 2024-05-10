6b122bd8612769ad24d237988b4628cc27b9da97
Jordan <jordanveg@gmail.com>
2024-05-10 18:14:07 -0400

    added basic card and player ability to shoot

---
+++ characters/cards/basic_card/basic_card.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var speed: int = 250
+@export var dmg: int = 1
+
+
+func spawn(pos, deg):
+	position = pos
+	rotation_degrees = deg
+	
+func _physics_process(delta):
+	position += transform.x * speed * delta
+
+func _on_body_entered(body):
+	if body.is_in_group("enemies"):
+		body.take_damage(self.dmg)
+	queue_free()
