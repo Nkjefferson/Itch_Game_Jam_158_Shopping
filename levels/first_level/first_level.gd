7808c850762c6628f35eac8945f87593aebdcb8c
Jordan <jordanveg@gmail.com>
2024-05-10 16:21:35 -0400

    removed renamed dirs

--- levels/first_level/first_level.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	MusicManager.play_music("res://assets/audio/House In a Forest Loop Reversed.ogg")
-
