c1f75931d42535f162ac65f0becc1dd06d3b1ca1
Jordan <jordanveg@gmail.com>
2024-05-10 15:42:18 -0400

    created scalable bookshelves with collision boxes

--- views/main_menu/main_menu_hud.gd
+++ views/main_menu/main_menu_hud.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 
 func _on_start_btn_pressed():
-	get_tree().change_scene_to_file("res://levels/first_level/first_level.tscn")
+	get_tree().change_scene_to_file("res://environment/first_level/first_level.tscn")
 
 # Called when the settings button is pressed.
 func _on_settings_btn_pressed():
