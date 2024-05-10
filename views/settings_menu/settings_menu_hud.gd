a479856d5bcc076cc12d83d6e4915cef4c3d2624
Nathaniel Jefferson <nathaniel.jefferson@gmail.com>
2024-05-10 09:10:58 -0400

    Updated menus to use layout tools

--- views/settings_menu/settings_menu_hud.gd
+++ views/settings_menu/settings_menu_hud.gd
@@ -6,12 +6,12 @@ var audio_settings = AudioSettingsMenu.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	audio_settings = GameState.load_game_state(audio_settings)
-	$Master_Volume_Slider.value = audio_settings.master_volume_slider_value
-	$Music_Volume_Slider.value = audio_settings.music_volume_slider_value
-	$Game_Volume_Slider.value = audio_settings.sound_effects_volume_slider_value
-	$Master_Volume_Mute_Toggle.button_pressed = audio_settings.master_volume_mute_toggle
-	$Music_Volume_Mute_Toggle.button_pressed = audio_settings.music_volume_mute_toggle
-	$Game_Volume_Mute_Toggle.button_pressed = audio_settings.sound_effects_volume_mute_toggle
+	$VBoxContainer/HBoxMaster/Master_Volume_Slider.value = audio_settings.master_volume_slider_value
+	$VBoxContainer/HBoxMusic/Music_Volume_Slider.value = audio_settings.music_volume_slider_value
+	$VBoxContainer/HBoxGame/Game_Volume_Slider.value = audio_settings.sound_effects_volume_slider_value
+	$VBoxContainer/HBoxMaster/Master_Volume_Mute_Toggle.button_pressed = audio_settings.master_volume_mute_toggle
+	$VBoxContainer/HBoxMusic/Music_Volume_Mute_Toggle.button_pressed = audio_settings.music_volume_mute_toggle
+	$VBoxContainer/HBoxGame/Game_Volume_Mute_Toggle.button_pressed = audio_settings.sound_effects_volume_mute_toggle
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -80 + audio_settings.master_volume_slider_value)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), -80 + audio_settings.music_volume_slider_value)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), -80 + audio_settings.sound_effects_volume_slider_value)
@@ -25,15 +25,15 @@ func _on_exit_btn_pressed():
 	get_tree().change_scene_to_file("res://views/main_menu/main_menu.tscn")
 
 func _on_master_volume_slider_value_changed(value):
-	audio_settings.master_volume_slider_value = $Master_Volume_Slider.value
+	audio_settings.master_volume_slider_value = $VBoxContainer/HBoxMaster/Master_Volume_Slider.value
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -80 + audio_settings.master_volume_slider_value)
 	
 func _on_music_volume_slider_value_changed(value):
-	audio_settings.music_volume_slider_value = $Music_Volume_Slider.value
+	audio_settings.music_volume_slider_value = $VBoxContainer/HBoxMusic/Music_Volume_Slider.value
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), -80 + audio_settings.music_volume_slider_value)
 
 func _on_game_volume_slider_value_changed(value):
-	audio_settings.sound_effects_volume_slider_value = $Game_Volume_Slider.value
+	audio_settings.sound_effects_volume_slider_value = $VBoxContainer/HBoxGame/Game_Volume_Slider.value
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), -80 + audio_settings.sound_effects_volume_slider_value)
 
 func _on_master_volume_mute_toggle_toggled(button_pressed):
