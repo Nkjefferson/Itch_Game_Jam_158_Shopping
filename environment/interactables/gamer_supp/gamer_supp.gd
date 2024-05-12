e277ebe6d55baebbb77cc5699fdbaec12bf712db
Nathaniel Jefferson <nathaniel.jefferson@gmail.com>
2024-05-12 00:22:02 -0400

    Implement interactable cooldowns

--- environment/interactables/gamer_supp/gamer_supp.gd
+++ environment/interactables/gamer_supp/gamer_supp.gd
@@ -5,6 +5,7 @@ extends Interactable
 func interact():
 	var player_inventory = player_body.get_node("Inventory")
 	if player_inventory.check_funds(get_parent().shop_cost):
+		super()
 		player_body.heal(juice_quality)
 		player_inventory.pay_gold(get_parent().shop_cost)
 		if interact_sound:
