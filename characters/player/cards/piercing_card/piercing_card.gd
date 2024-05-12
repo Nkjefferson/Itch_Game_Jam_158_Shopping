d534540578e04d9dd28db79815ca0fd1d807da21
Nathaniel Jefferson <nathaniel.jefferson@gmail.com>
2024-05-12 17:55:52 -0400

    Added knockback from card hits

--- characters/player/cards/piercing_card/piercing_card.gd
+++ characters/player/cards/piercing_card/piercing_card.gd
@@ -7,7 +7,7 @@ var bodies_encountered : Array[CharacterBody2D]
 func _on_body_entered(body):
 	if body.is_in_group("enemies"):
 		if !bodies_encountered.has(body):
-			body.take_damage(self.damage)
+			body.take_damage(self)
 			enemies_to_pierce -= 1
 			# Level the audio based on distance from player on impact
 			MusicManager.play_sound_effect(enemy_hit_sound,-abs((global_position-parent_object.global_position).length())/25)
