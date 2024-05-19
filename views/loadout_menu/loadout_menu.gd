e3bbf68969deb2aa60d2225d35a17b1e843a4754
Jordan <jordanveg@gmail.com>
2024-05-19 00:42:41 -0400

    fixed some alignment issues of grid container

--- views/loadout_menu/loadout_menu.gd
+++ views/loadout_menu/loadout_menu.gd
@@ -12,19 +12,19 @@ func _ready():
 	update_grid(temp_inventory)
 	
 	for ability_card in $Layout/Hotbar.get_children():
-		ability_card.get_node("VBoxContainer/SelectableTile").connect("sig_take",$CurrentSelectionPanel._take_card_from_tile)
-		ability_card.get_node("VBoxContainer/SelectableTile").connect("sig_give",$CurrentSelectionPanel._give_card_to_tile)
+		ability_card.get_node("SelectableTile").connect("sig_take",$CurrentSelectionPanel._take_card_from_tile)
+		ability_card.get_node("SelectableTile").connect("sig_give",$CurrentSelectionPanel._give_card_to_tile)
 
 func set_hotkey_labels():
 	var i = 2
 	for ability_card in $Layout/Hotbar.get_children():
-		ability_card.get_node("VBoxContainer/HotkeyLabel").text = InputMap.action_get_events("ActionButton" + str(i))[0].as_text().get_slice(" ",0)
+		ability_card.get_node("HotkeyLabel").text = InputMap.action_get_events("ActionButton" + str(i))[0].as_text().get_slice(" ",0)
 		i+=1
 
 func set_action_bar_loadout(loadout:Array[PackedScene]):
 	for i in range(0,loadout.size()):
 		if loadout[i]:
-			$Layout/Hotbar.get_node("Hotbar_Tile" + str(i+2)).get_node("VBoxContainer/SelectableTile").set_card(loadout[i])
+			$Layout/Hotbar.get_node("Hotbar_Tile" + str(i+2)).get_node("SelectableTile").set_card(loadout[i])
 
 func update_grid(inventory:Array[PackedScene]):
 	for i in range(0, inventory.size()):
