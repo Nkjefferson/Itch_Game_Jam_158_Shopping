f340d6dc9c622bf58b2ab8d400c3107c001965d9
Nathaniel Jefferson <nathaniel.jefferson@gmail.com>
2024-05-31 20:08:21 -0400

    Merge in card loadout updates and also update look and feel of tooltip

--- views/player_hud/action_button/action_button.gd
+++ views/player_hud/action_button/action_button.gd
@@ -1,6 +1,6 @@
 extends DisplayTile
 
-@export var card_scene : PackedScene = null
+@onready var tooltip = $CanvasLayer/Control/TooltipPane
 
 var greyed_out_shader_material
 
@@ -9,6 +9,21 @@ func _ready():
 	super()
 	greyed_out_shader_material = ShaderMaterial.new()
 	greyed_out_shader_material.shader = load("res://views/player_hud/action_button/grey_scale.gdshader")
+
+func set_card(card_scene):
+	super(card_scene)
+	if scene:
+		if sprite:
+			tooltip.set_texture(sprite.texture,sprite_frame_count)
+		tooltip.set_title(scene.card_info.card_name)
+		tooltip.add_subtitle("Pack: " + ExpansionSet.display_string(scene.card_info.pack))
+		tooltip.add_subtitle("Rarity: " + Rarity.display_string(scene.card_info.rarity))
+		
+		# Await a process frame so the size can be accurately reflected
+		tooltip.visible = true
+		await get_tree().process_frame
+		tooltip.move(Vector2(20,-tooltip.get_height()-20))
+		tooltip.visible = false
 
 func set_selected(selected):
 	var style;
@@ -27,3 +42,15 @@ func set_count(num):
 		set_sprite_material(greyed_out_shader_material)
 	else:
 		set_sprite_material(null)
+
+func display_tooltip():
+	tooltip.visible = true
+	
+func clear_tooltip():
+	tooltip.visible = false
+
+func _on_panel_mouse_entered():
+	display_tooltip()
+
+func _on_panel_mouse_exited():
+	clear_tooltip()
