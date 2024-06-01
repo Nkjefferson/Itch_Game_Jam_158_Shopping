42e0c543b31bdc3861a303414c6b64b7eef86768
Nathaniel Jefferson <nathaniel.jefferson@gmail.com>
2024-05-31 20:39:37 -0400

    Added more dynamic player healthbar

--- views/player_hud/player_hud.gd
+++ views/player_hud/player_hud.gd
@@ -1,26 +1,29 @@
 extends CanvasLayer
 
+@onready var healthbar = $HUD/HealthBar
+@onready var actionbar = $HUD/Actionbar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_hotkey_labels()
+	healthbar.init_health(100)
 
 func set_hotkey_labels():
-	for ability_card in $HUD/Actionbar.get_children():
+	for ability_card in actionbar.get_children():
 		ability_card.get_node("Panel/HotkeyLabel").text = InputMap.action_get_events(ability_card.name)[0].as_text().get_slice(" ",0)
 
 func set_action_bar_loadout(loadout:Array[CardSlot]):
 	for i in range(0,loadout.size()):
 		if loadout[i]:
-			$HUD/Actionbar.get_node("ActionButton" + str(i+1)).set_card(loadout[i].card)
+			actionbar.get_node("ActionButton" + str(i+1)).set_card(loadout[i].card)
 
 func _update_card_hotbar(index, count):
-	var hotkey_labels = $HUD/Actionbar.get_children()
+	var hotkey_labels = actionbar.get_children()
 	hotkey_labels[index].set_count(count)
 
 func _update_health_value(health):
-	$HUD/HealthBar/ValueProgressBar/Value.text = str(health) + "%"
-	$HUD/HealthBar/ValueProgressBar.value = health
+	#healthbar.text = str(health) + "%"
+	healthbar.health = health
 	
 func _update_score(score):
 	$HUD/ScoreTicker/Value.text = str(score)
